13f46a1a3d263b1b8163aab763ae85ac0af7e4e4
Jacob Swenson <jswens912@gmail.com>
2019-10-07 17:17:43 -0500

    I actually did it

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const EE = require('./hub');
-const fileWrite = require('./writeFiles');
-const upperCase = require('./upperCase');
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let newData = upperCase(data);
-    fileWrite(file,newData);
-    });
-  };
+
+const alterFile = require('./alterFile');
 
 let file = process.argv.slice(2).shift();
 alterFile(file);
